f295934ec08bfa3880c46ac95ec1602b2bb863fb
Oscarvg27 <oscarvg200@gmail.com>
2023-11-23 13:56:35 -0600

    commit de Oscar

---
+++ Modelos/Datos.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class usuarios
+    {
+        public string correo { get; set; }
+        public string password { get; set; }
+    }
+
+    public class cliente
+    {
+        public string nombre { get; set; }
+        public string ap_materno { get; set; }
+        public string ap_paterno { get; set; }
+    }
+
+    public class empresa
+    {
+        public string nombre { get; set;}
+    }
+
+    public class direccion
+    {
+        public string calle { get; set; }
+        public int numero { get; set; }
+        public string colonia { get; set; }
+        public int codigopostal { get; set; }
+        public string estado { get; set; }
+    }
+
+    public class rutas
+    {
+        public float totalviaje { get; set; }
+    }
+
+    public class unidades
+    {
+        public string placa { get; set; }
+        public int cant_carga { get; set; }
+        public string marca { get; set; }
+        public int numremolques { get; set; }
+    }
+
+    public class operadores
+    {
+        public string nombre { get; set; }
+        public string ap_materno { get; set; }
+        public string ap_paterno { get; set; }
+        //public int cve_inventario { get; set; }
+        public int num_licencia { get; set; }
+    }
+
+    public class director
+    {
+        public string nombre { get; set; }
+        public string ap_materno { get; set; }
+        public string ap_paterno { get; set; }
+    }
+
+    public class tipo_reporte
+    {
+        public string nombre { get; set; }
+    }
+
+    public class GPS
+    {
+        public string no_serie { get; set; }
+    }
+
+    public class incidentes
+    {
+        public string descripcion { get; set; }
+    }
+
+
+        
+}
---
+++ Modelos/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelos
+{
+    public class Singleton
+    {
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Singleton();
+                return _instance;
+            }
+        }
+
+        public SqlConnection conectarBase()
+        {
+            SqlConnection cnn;
+            string connectionString = @"Data Source=LAPTOP-PUSDDRN8\SQLEXPRESS ; Initial Catalog = SMARTSHIP; User ID=Jenny;Password = hola123;TrustServerCertificate=True";
+
+            cnn = new SqlConnection(connectionString);
+
+            cnn.Open();
+
+            //Comprobar
+            //MessageBox.Show("Connection Open! ");
+            return cnn;
+        }
+    }
+}
